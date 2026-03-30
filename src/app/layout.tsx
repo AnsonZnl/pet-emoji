ac9eaaa78d17e0b0a1fe3c0e17a0eadca0b1a56b
zhangningle.2025 <zhangningle.2025@jd.com>
2026-03-30 09:55:44 +0800

    Remove Google Ads script from layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -126,8 +126,6 @@ export default function RootLayout({
           }}
         />
         
-        <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4405660368108333"
-     crossorigin="anonymous"></Script>
       </body>
     </html>
   );
